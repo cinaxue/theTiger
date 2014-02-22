6b9fd3076f5d30a374cad43874f69957ef2c85f9
Cina Xue <85xueqian@163.com>
2014-02-22 17:05:50 +0800

    test01 - Cina

--- Louder, Please!/Louder, Please!/main.m
+++ Louder, Please!/Louder, Please!/main.m
@@ -13,6 +13,8 @@
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
+        
+        NSLog(@"Learn Git");
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
